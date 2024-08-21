795af65a1de5a8b2056cfb630742cee43eff5fe9
Vinicius Coutinho <viniciusoliveiracoutinho@gmail.com>
2024-08-20 23:34:02 -0300

    fifo, lifo, priority

---
+++ Semana3Semestre2/src/main/java/org/example/desafio_semana_1/FIla.java
@@ -0,0 +1,60 @@
+package org.example.desafio_semana_1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class FIla {
+    public static void main(String[] args) {
+
+//        Queue<String> fila = new LinkedList<>();
+//
+//        fila.add("maria");
+//        fila.add("João");
+//        fila.add("Pedro");
+//        fila.add("Carlos");
+//        fila.add("Ana");
+//
+//
+//        System.out.println(fila.size());
+//
+//        //consulta para verificar o proxima
+//        System.out.println(fila.peek());
+//
+//        System.out.println(fila.size());
+//
+//        //chamar o proximo, recupera e remove da fila
+//        System.out.println(fila.poll());
+//
+//        System.out.println(fila.size());
+//
+//        System.out.println(fila.poll());
+//
+//        System.out.println(fila.size());
+//
+//        fila.remove("carlos");
+
+
+//        Stack<String> nomes = new Stack<>();
+//
+//        nomes.push("Maria");
+//        nomes.push("Joao");
+//        nomes.push("Pedro");
+//        nomes.push("Carlos");
+//        nomes.push("Ana");
+//
+//        System.out.println(nomes.size());
+//
+//        System.out.println(nomes.peek());
+//
+//        System.out.println(nomes.pop());
+//
+//        System.out.println(nomes.size());
+//
+
+
+
+
+    }
+
+}
